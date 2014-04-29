ccc2da45e6e954667cde0f58f4d468ae41cb0d22
krzysztof.balonek <krzysztof.balonek@gmail.com>
2014-04-29 14:09:25 +0100

    BerlinClock parses input string using JodaTime.

--- src/main/java/com/inkglobal/techtest/BerlinClock.java
+++ src/main/java/com/inkglobal/techtest/BerlinClock.java
@@ -1,4 +1,20 @@
 package com.inkglobal.techtest;
 
+import org.joda.time.LocalTime;
+
 public class BerlinClock {
+    private final LocalTime time;
+
+    /**
+     *
+     * @param time in format "HH:MM:SS"
+     *
+     */
+    public BerlinClock(String input) {
+        this.time = new LocalTime(input);
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
 }
--- src/test/java/com/inkglobal/techtest/BerlinClockTest.java
+++ src/test/java/com/inkglobal/techtest/BerlinClockTest.java
@@ -1,6 +1,53 @@
 package com.inkglobal.techtest;
 
+import org.joda.time.LocalTime;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class BerlinClockTest {
+
+    @Test
+    public void should_parse_the_0AM_input_string() {
+        // given
+        String input = "00:00:00";
+
+        // when
+        BerlinClock clock = new BerlinClock(input);
+
+        // then
+        LocalTime expectedTime = new LocalTime(input);
+        assertThat(clock.getTime(), is(equalTo(expectedTime)));
+        assertThat(clock.getTime().getHourOfDay(), is(equalTo(0)));
+        assertThat(clock.getTime().getMinuteOfHour(), is(equalTo(0)));
+        assertThat(clock.getTime().getSecondOfMinute(), is(equalTo(0)));
+    }
+
+    @Test
+    public void should_parse_the_13_17_01_input_string() {
+        // given
+        String input = "13:17:01";
+
+        // when
+        BerlinClock clock = new BerlinClock(input);
+
+        // then
+        LocalTime expectedTime = new LocalTime(input);
+        assertThat(clock.getTime(), is(equalTo(expectedTime)));
+        assertThat(clock.getTime().getHourOfDay(), is(equalTo(13)));
+        assertThat(clock.getTime().getMinuteOfHour(), is(equalTo(17)));
+        assertThat(clock.getTime().getSecondOfMinute(), is(equalTo(1)));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_throw_invalid_arg_exception_for_invalid_output() {
+        // given
+        String input = "13:17:01:21";
+
+        // when
+        BerlinClock clock = new BerlinClock(input);
+
+        // then: exception should be thrown
+    }
 }
